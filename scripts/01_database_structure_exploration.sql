2e85a200d8204d91cc49427afa361b628e157efc
yurirore <0360876@sd.taylors.edu.my>
2026-06-21 17:53:39 +0800

    Delete scripts/01_database_structure_exploration.sql

--- scripts/01_database_structure_exploration.sql
+++
@@ -1,6 +0,0 @@
---explore all objects in the database
-SELECT * FROM INFORMATION_SCHEMA.TABLES
-
--- explore all columns in the database
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'dim_customers'
